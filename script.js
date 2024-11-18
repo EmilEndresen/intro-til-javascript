829bf80990425d931c087f402c7f14a97b81a176
Emil Endresen <emil@jobloop.no>
2024-11-18 10:17:16 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("Velkommen til en introduksjon til  javascript")
+
+// Variabler
+
+// let x = 5
+// console.log(x)
+
+// const y = 10
+// console.log(y)
+
+// I javascript bruker vi camelCase
+
+const introHeader = document.getElementById("intro-header")
+console.log(introHeader)
+
+// Datatyper
+
+// String
+let string = "Tekst"
+
+// Integer/Number
+let number = 10
+
+// Float
+let float = 10.5
+
+// Boolean
+let boolean = true
+console.log(boolean)
+
+let darkModeToggle = false
+
+// Array
+const fruits = ["Apple", "Orange", "Pear", "Banana"]
+console.log(fruits)
+
+const arrayNumbers = ["18", 15, 11.7]
+console.log(arrayNumbers)
+
+// Object
+const person = {
+   firstName: "Emil",
+   lastName: "Endresen",
+   age: 30,
+   hobbies: ["Disc Golf", "DnD", "Fisking", "Film"]
+}
+console.log(person)
+
+// Operators
+
+let x = 6
+let y = 20
+
+let z = x * y
+console.log(z)
